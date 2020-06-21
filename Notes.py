7ab11d0bdf0282ac34eccfcefb46ec8c87ce6ed8
Rahul Chauhan <rahulsp30101@gmail.com>
2020-06-21 16:19:49 +0530

    Initial Commit

---
+++ Notes.py
@@ -0,0 +1,17 @@
+1. Https Verbs 
+    ->Allows creation of interaction b/w to connected elements.
+    -> GET means to recieve something.
+    -> POST means to send something.
+    -> PUT makes sure something is there.
+    -> DELETE removes something.
+2. Web Server
+    -> Software to accept incoming web requests.
+3. A GET Request
+    -> GET / HTTP/1.1
+    -> GET -> is a verb 
+    -> / denotes the path
+
+4. REST Principles
+    -> REST API - How a web server behaves in general. It responds with resources rather than just the data.
+    -> REST is stateless. Requests does not depend on other requests.
+    
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Rest-API-Using-Flask
+This repositry I'll be pushing codes with explaination while I learn how to create Rest API using flask and python.
